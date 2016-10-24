7c2504e179058decd43ea33cb169480eefc7189b
Sebastián González Parra <sebagonzalez18@hotmail.com>
2016-10-23 23:37:38 -0300

    se crea plantilla para los php

---
+++ ColegioAppPhp/visitante.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title> Colegio App</title>
+	<meta name="description" content="">
+	<meta name="Franco Valerio, Gonzalo Zeballos, Sebastian Gonzalez, Roberto Hormazabal" content="">
+	<link rel="icon" href="../../favicon.ico">
+	
+	<!-- Bootstrap -->
+	<!-- <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"> -->
+	<link rel="stylesheet" href="Content/bootstrap.min.css">
+	<!-- Custom styles-->
+	<link href="Content/cover.css" rel="stylesheet">
+	<link href="Content/style.css"  rel="stylesheet" type="text/css" /> 
+</head>
+
+<body>
+
+<?php 
+// aqui si necesitas usar el php
+?>
+	<!-- Bootstrap core JavaScript
+	  ================================================== -->
+	<!-- <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script> -->
+	<script src="Scripts/bootstrap.min.js"></script>
+
+		<!-- AngularJS y Jquery -->
+	<script src= "http://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
+	<script src="Scripts/jquery-3.1.1.min.js" type="text/javascript"></script>
+	<script src="Scripts/jquery-ui-1.12.1.min.js" type="text/javascript"></script>
+	</body>
+</html>
+
