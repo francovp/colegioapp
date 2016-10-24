9395723cdc94f76a35b5383a733a3dca625b6fba
Sebastián González Parra <sebagonzalez18@hotmail.com>
2016-10-24 02:22:08 -0300

    prueba visitante
    visitante

--- ColegioAppPhp/visitante.php
+++ ColegioAppPhp/visitante.php
@@ -20,7 +20,39 @@
 <body>
 
 <?php 
-// aqui si necesitas usar el php
+	// aqui si necesitas usar el php
+	
+	//conexion a la DB
+	require('database.php');
+	//Se obtienen datos de la DB para mostrarlas en el perfil
+	$sql = "SELECT * FROM colegio ;";
+	$result = pg_query($conexion,$sql);
+	$num_filas = pg_num_rows($result);
+
+	echo '<select id="ingresoSexo" class="form-control" name="sexo">';
+	while($fila= pg_fetch_row($result)){
+		echo '<option>'.$fila[1].'</option>';
+
+	}
+	echo '</select><br>';
+
+
+	
+	
+	/* Franco se encarga de la sesion
+	if (!isset($_SESSSION)) {
+		session_start();
+	}
+	$_SESSION["username"] = $tupla[1];
+	$_SESSION["nombre"] = $tupla[2];
+	$_SESSION["apellido"] = $tupla[3];
+	$_SESSION["sexo"] =$tupla[4];
+	$_SESSION["email"] = $tupla[5];
+	$_SESSION["pass"] = $tupla[6];
+	*/
+
+	//Se cierra conexi{on a la DB
+	pg_close($conexion);
 ?>
 	<!-- Bootstrap core JavaScript
 	  ================================================== -->
@@ -31,6 +63,7 @@
 	<script src= "http://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
 	<script src="Scripts/jquery-3.1.1.min.js" type="text/javascript"></script>
 	<script src="Scripts/jquery-ui-1.12.1.min.js" type="text/javascript"></script>
-	</body>
+</body>
+
 </html>
 
