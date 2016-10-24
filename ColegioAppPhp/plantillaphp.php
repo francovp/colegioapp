148f4e0c7178ed97a544ac0ba81b1556c95c8960
Sebastián González Parra <sebagonzalez18@hotmail.com>
2016-10-24 01:13:29 -0300

    arreglado pequeño error

--- ColegioAppPhp/plantillaphp.php
+++ ColegioAppPhp/plantillaphp.php
@@ -31,5 +31,5 @@
 	<script src= "http://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
 	<script src="Scripts/jquery-3.1.1.min.js" type="text/javascript"></script>
 	<script src="Scripts/jquery-ui-1.12.1.min.js" type="text/javascript"></script>
-	</body>
+</body>
 </html>
